71455d9a15680353f57a5386480a2065f598957b
Jennifer Bland <jennifer.bland@sbdinc.com>
2017-06-24 09:24:13 -0400

    removing unneeded files

--- example-05/app.js
+++ example-05/app.js
@@ -4,20 +4,20 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  // res.send('home page');
-  res.sendFile('index.html', { root: __dirname });
+  res.send('home page');
+  // res.sendFile('index.html', { root: __dirname });
 });
 app.get('/about', function(req, res) {
-  // res.send('about page');
-  res.sendFile('about.html', { root: __dirname });
+  res.send('about page');
+  // res.sendFile('about.html', { root: __dirname });
 });
 app.get('/contact', function(req, res) {
-  // res.send('contact page');
-  res.sendFile('contact.html', { root: __dirname });
+  res.send('contact page');
+  // res.sendFile('contact.html', { root: __dirname });
 });
 app.get('/products', function(req, res) {
-  // res.send('products page');
-  res.sendFile('products.html', { root: __dirname });
+  res.send('products page');
+  // res.sendFile('products.html', { root: __dirname });
 });
 
 app.listen(3000, function() {
